d5f7bb50f889f3256fc1c515137e5b4bd8b265d5
Janek <Janek@DESKTOP-03ULGT4>
2023-01-02 17:10:40 +0100

    Dodaj pliki projektów.

---
+++ ZADANIE 1 2021/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZADANIE_1_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    if (i<20&&j<20)
+                    {
+                        Console.Write("*");
+
+                    }
+                    else if (i >= 20 && j < 20)
+                    {
+                        Console.Write("@");
+                    }
+                    else if (i < 20 && j >= 20)
+                    {
+                        Console.Write("#");
+                    }
+                    else if (i >= 20 && j >= 20)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (i >= 20 && j >= 20)
+                    {
+                        Console.Write("&");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
